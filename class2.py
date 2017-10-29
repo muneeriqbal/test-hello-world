a7d62c193ef772d334e7dd6c9693183770db743c
muneeriqbal <33184904+muneeriqbal@users.noreply.github.com>
2017-10-30 01:09:18 +0500

    Add files via upload

---
+++ class2.py
@@ -0,0 +1,19 @@
+def myfunc(s):
+    for lassi in range(1,s):
+        if lassi>5:
+            print("lassi" + str(lassi))
+            print("you win")
+
+    else:
+            print("you losse")
+
+
+
+
+
+myfunc(9)
+
+
+
+
+
