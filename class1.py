b5bc4ac754ef91ea87facdc62c0d1bbc9846f36f
muneeriqbal <33184904+muneeriqbal@users.noreply.github.com>
2017-10-29 23:45:15 +0500

    Add files via upload

---
+++ class1.py
@@ -0,0 +1,17 @@
+def findprime(upto):
+ for a in range(1,upto):
+     if a%2:
+         print(a , "prime number")
+
+
+
+findprime(100)
+
+
+def findodd(upto):
+    for b in range(1,upto):
+        if b%2:
+            print(b, "odd")
+
+
+findodd(100)
