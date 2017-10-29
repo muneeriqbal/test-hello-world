3384505f86cb7689ee43092bcdc362c23ee801e6
muneeriqbal <33184904+muneeriqbal@users.noreply.github.com>
2017-10-29 23:30:01 +0500

    Add files via upload

---
+++ helloworld.py
@@ -0,0 +1,33 @@
+
+a = "salam"
+a = 5
+print(a)
+
+
+b = "moiz"
+c= "syed"
+c = c+b
+print(c)
+
+
+num = "5"
+num1 = 6
+plus = int(num) + num1
+print(plus)
+
+
+boleans = 5<6
+print(boleans)
+
+
+pizza = 3
+if pizza<2:
+    print("sorry")
+else:
+    print("welcome")
+
+
+
+
+z = range(1,3)
+print(z)
